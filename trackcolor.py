70b3363e625e81fd47f07cfbdcc5330fb1fc600e
Max Upravitelev <max@upravitelev.de>
2021-02-18 18:45:15 +0100

    refactor moving camera by detecting direction

--- trackcolor.py
+++ trackcolor.py
@@ -27,13 +27,15 @@ while True:
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
     # red color
-    low_red = np.array([161, 155, 84])
+    # low_red = np.array([161, 155, 84])
+    # high_red = np.array([179, 255, 255])
+    low_red = np.array([161, 60, 84])
     high_red = np.array([179, 255, 255])
-
 #     low_red = np.array([0, 0, 0])
 #     high_red = np.array([20, 20, 20])
 
     red_mask = cv2.inRange(hsv_frame, low_red, high_red)
+    
     # print(red_mask)
     contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
     contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse=True)
@@ -42,14 +44,14 @@ while True:
         if cv2.contourArea(cnt) > 1000:
             (x, y, w, h) = cv2.boundingRect(cnt)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
-            last_x_medium = x_medium
-            x_medium = int((x + x + w) / 2)
+            #last_x_medium = x_medium
+            #x_medium = int((x + x + w) / 2)
 
-            if last_x_medium > x_medium:
+            if x < x_medium:
                 print("left")
                 motor.move_non_threaded(1, "left")
                 #time.sleep(0.1)
-            elif last_x_medium == x_medium:
+            elif x == x_medium:
                 print("stay")
             else:
                 print("right")
