4adeba6d677ffc5006b1b4afae4191c42988a169
Max Upravitelev <max@upravitelev.de>
2021-02-21 20:28:03 +0100

    handle navigation in canvas module

--- draw_threaded.py
+++ draw_threaded.py
@@ -3,6 +3,7 @@ import numpy as np
 import time
 import json
 from modules.painter import Painter
+from modules.canvas import Canvas_painter
 
 # function to parse bool value from config file
 from utils.boolcheck import boolcheck
@@ -18,96 +19,90 @@ with open(config_path) as config_file:
 enable_fps_timer = boolcheck(config["general_config"]["enable_fps_timer"])
 
 
-# camera_mode = "picam"
+camera_mode = "webcam"
 
-# # init videostream (separate thread)
-# if camera_mode == "webcam":
+# init videostream (separate thread)
+if camera_mode == "webcam":
 
-#     # start raspivid in a subprocess
-#     import subprocess
-#     cmd = "raspivid -n -t 0 -n  -w 1024 -h 768  -ih -fl -l -o - | /bin/nc -lvp 5000"
-#     subprocess.Popen(cmd, shell=True)
-#     from modules.cam import VideoStream
-#     time.sleep(2)
-#     cap = VideoStream(src=0).start()
-# else: 
-#     from modules.PiCam import PiCam 
-#     cap = PiCam().start()
+    # start raspivid in a subprocess
+    # import subprocess
+    # cmd = "raspivid -n -t 0 -n  -w 1024 -h 768  -ih -fl -l -o - | /bin/nc -lvp 5000"
+    # subprocess.Popen(cmd, shell=True)
+    from modules.cam import VideoStream
+    time.sleep(2)
+    cap = VideoStream(src=0).start()
+else: 
+    from modules.PiCam import PiCam 
+    cap = PiCam().start()
 
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 # # Set camera resolution
 
-cap_width = 480
-cap_height = 320
-cap.set(3, cap_width)
-cap.set(4, cap_height)
+# cap_width = 480
+# cap_height = 320
+# cap.set(3, cap_width)
+# cap.set(4, cap_height)
 
+cap_dimensions = [480, 320]
+canvas = Canvas_painter(cap_dimensions).start()
 
 time.sleep(1.0)
 #_, black_frame = cap.read()
 
-frame_width = 1024
-frame_height = 768
 
-resize_width_factor = frame_width / cap_width
-resize_heigth_factor = frame_height / cap_height
-reset_factors =  [resize_width_factor, resize_heigth_factor]
+# frame_width = 640
+# frame_height = 480
 
-black_frame = np.zeros((frame_height, frame_width, 3), np.uint8)
-print(frame_width)
-print(frame_height)
+# resize_width_factor = frame_width / cap_width
+# resize_heigth_factor = frame_height / cap_height
+# reset_factors =  [resize_width_factor, resize_heigth_factor]
 
-cv2.rectangle(black_frame, (0, 0), (frame_width, frame_height), (0, 0, 0), -1)
+# black_frame = np.zeros((frame_height, frame_width, 3), np.uint8)
+# print(frame_width)
+# print(frame_height)
+
+# cv2.rectangle(black_frame, (0, 0), (frame_width, frame_height), (0, 0, 0), -1)
 
 
 if enable_fps_timer == True:
     timer2 = time.time()
 
+# reset_area_width = 50
+# reset_area_height = 50
+# save_area_width = 50
+# save_area_height = 50
+# save_area_y = frame_height - 50
 
+# def reset_canvas():
+#     cv2.rectangle(black_frame, (0, 0), (frame_width, frame_height), (0, 0, 0), -1)
+#     cv2.rectangle(black_frame, (0, 0), (reset_area_width, reset_area_height), (10, 10, 10), -1)
+#     cv2.rectangle(black_frame, (0, save_area_y), (save_area_width, save_area_y + save_area_height), (20, 20, 20), -1)
 
-reset_area_width = 50
-reset_area_height = 50
-save_area_width = 50
-save_area_height = 50
-save_area_y = frame_height - 50
+# reset_canvas()
 
-def reset_canvas():
-    cv2.rectangle(black_frame, (0, 0), (frame_width, frame_height), (0, 0, 0), -1)
-    cv2.rectangle(black_frame, (0, 0), (reset_area_width, reset_area_height), (10, 10, 10), -1)
-    cv2.rectangle(black_frame, (0, save_area_y), (save_area_width, save_area_y + save_area_height), (20, 20, 20), -1)
-
-reset_canvas()
-
-painter = Painter(black_frame, reset_factors).start()
+painter = Painter(canvas.black_frame, canvas.resize_factors).start()
 
 
 counter = 0
 
 while True:
 
-
     if enable_fps_timer == True:
         timer1 = time.time()
 
-    _, frame = cap.read()
+    frame = cap.read()
     frame = cv2.flip(frame, 1)
 
     painter.frame = frame
 
+    black_frame = canvas.black_frame
+
     cv2.circle(black_frame, (painter.brush_x, painter.brush_y), painter.brush_radius, (255, 255, 255), -1)
 
-    if painter.brush_x < reset_area_width and painter.brush_y < reset_area_height:
-        reset_canvas()
-        time.sleep(1)
-
-    if painter.brush_x < save_area_width and painter.brush_y > save_area_y:
-        counter += 1
-        localPath = 'images/image1000'+str(counter)+'.jpg'
-        cv2.imwrite(localPath,black_frame)
-        time.sleep(1)
-
-
+    # canvas.painter_brush_x = painter.brush_x
+    # canvas.painter_brush_y = painter.brush_y
+    
     if enable_fps_timer == True:
         print("FPS: " + str(1/((timer1-timer2))))
         timer2 = time.time()
---
+++ modules/canvas.py
@@ -0,0 +1,76 @@
+from threading import Thread
+import cv2
+import imutils
+import json
+import time
+import numpy as np
+
+# function to parse bool value from config file
+from utils.boolcheck import boolcheck
+
+class Canvas_painter:
+    def __init__(self, frame, cap_dimensions=[640,480]):
+        
+        # get settings from config file
+        config_path = 'config/config.json'
+
+        with open(config_path) as config_file:
+            config = json.load(config_file)
+
+        # init thread handling
+        self.stopped = False
+
+        # set verbose mode
+        self.verbose = boolcheck(config["general_config"]["verbose"])
+
+        self.frame_width = 640
+        self.frame_height = 480
+
+        self.resize_width_factor = self.frame_width / cap_dimensions[0]
+        self.resize_heigth_factor = self.frame_height / cap_dimensions[1]
+        self.resize_factors = [self.resize_width_factor, self.resize_heigth_factor]
+
+        self.black_frame = np.zeros((self.frame_height, self.frame_width, 3), np.uint8)
+
+        self.reset_area_width = 50
+        self.reset_area_height = 50
+
+        self.save_area_width = 50
+        self.save_area_height = 50
+        self.save_area_y = self.frame_height - 50
+
+        self.painter_brush_x = 0
+        self.painter_brush_y = 0
+
+        self.reset_canvas()
+
+    def reset_canvas(self):
+        cv2.rectangle(self.black_frame, (0, 0), (self.frame_width, self.frame_height), (0, 0, 0), -1)
+        cv2.rectangle(self.black_frame, (0, 0), (self.reset_area_width, self.reset_area_height), (10, 10, 10), -1)
+        cv2.rectangle(self.black_frame, (0, self.save_area_y), (self.save_area_width, self.save_area_y + self.save_area_height), (20, 20, 20), -1)
+
+            
+    def start(self):    
+        Thread(target=self.paint, args=()).start()
+        return self    
+
+    def paint(self):
+        while not self.stopped:
+            if self.painter_brush_x < self.reset_area_width and self.painter_brush_y < self.reset_area_height:
+                self.reset_canvas()
+                time.sleep(1)
+
+            if self.painter_brush_x < self.save_area_width and self.painter_brush_y > self.save_area_y:
+                counter += 1
+                localPath = 'images/image1000'+str(counter)+'.jpg'
+                cv2.imwrite(localPath,black_frame)
+                time.sleep(1)
+
+
+            # if cv2.waitKey(1) == ord("q"):
+            #     if self.verbose == True:
+            #         print("painter stopped")
+            #     self.stopped = True
+
+    def stop(self):
+        self.stopped = True
