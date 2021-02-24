b7d8ca6f52efda91cc11ffe832c1e8643a36b2fe
Max Upravitelev <max@upravitelev.de>
2021-02-24 17:07:41 +0100

    move tracking color into own function

--- README.md
+++ README.md
@@ -2,19 +2,27 @@
 
 ## about
 
-This package provides different smart mirror applications. All of them work with the same hardware setup: 
-- a screen connected to a machine running python
+This kit provides a set of different smart mirror applications. All of them work with the same hardware setup: 
+- a screen and a camera connected to a machine running python
 - a two-way mirror on top of the screen
-- 
 
 ## usage
 
 ### smart mirror painter
 
-run `python draw_threaded.py`
+run `python painter.py`
 
 ### smart mirror effects
 
-run `python draw_threaded.py`
+run `python effects.py`
 
+### smart mirror tracker
+
+run `python tracker.py`
+
+If you run this script on
+
+## current stage
+
+All of the scripts are runable but still in development at the moment. 
 
--- trackcolor.py
+++ trackcolor.py
@@ -2,20 +2,20 @@ import cv2
 import numpy as np
 import time
 
-gpio_motor = True
+gpio_motor = False
 if gpio_motor == True:
     from modules.gpio_motor import GPIO_motor
     motor = GPIO_motor()
 
-camera_mode = "picam"
+camera_mode = "webcam"
 
 # init videostream (separate thread)
 if camera_mode == "webcam":
 
-    # start raspivid in a subprocess
-    import subprocess
-    cmd = "raspivid -n -t 0 -n  -w 1024 -h 768  -ih -fl -l -o - | /bin/nc -lvp 5000"
-    subprocess.Popen(cmd, shell=True)
+    # # start raspivid in a subprocess
+    # import subprocess
+    # cmd = "raspivid -n -t 0 -n  -w 1024 -h 768  -ih -fl -l -o - | /bin/nc -lvp 5000"
+    # subprocess.Popen(cmd, shell=True)
     from modules.cam import VideoStream
     time.sleep(2)
     cap = VideoStream(src=0).start()
@@ -25,48 +25,57 @@ else:
 
 time.sleep(0.5)
 frame = cap.read()
-frame_width = frame.shape[0]
+frame_height = frame.shape[0]
+frame_width = frame.shape[1]
 
 x_medium = int(frame_width / 2)
 
-while True:
-    frame = cap.read()
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+def trackcolor(frame):
     
-    # red color
-    # low_red = np.array([161, 155, 84])
-    # high_red = np.array([179, 255, 255])
-    low_red = np.array([161, 155, 84])
-    high_red = np.array([179, 255, 255])
-#     low_red = np.array([0, 0, 0])
-#     high_red = np.array([20, 20, 20])
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    red_mask = cv2.inRange(hsv_frame, low_red, high_red)
+    # define color limits to be detected 
+    # red color
+    low_color_limit = np.array([161, 155, 84])
+    high_color_limit = np.array([179, 255, 255])
+
+    color_mask = cv2.inRange(hsv_frame, low_color_limit, high_color_limit)
     
     # print(red_mask)
-    contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+    contours, _ = cv2.findContours(color_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
     contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse=True)
+
+    cv2.rectangle(frame, (0, 0), (frame_width, frame_height), (0, 0, 0), -1)
 
     for cnt in contours:
         if cv2.contourArea(cnt) > 1000:
             (x, y, w, h) = cv2.boundingRect(cnt)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 3)
             #last_x_medium = x_medium
             #x_medium = int((x + x + w) / 2)
 
-            if x < x_medium:
-                print("left")
-                motor.move_non_threaded(1, "left")
-                #time.sleep(0.1)
-            elif x == x_medium:
-                print("stay")
-            else:
-                print("right")
-                motor.move_non_threaded(1, "right")
-                #time.sleep(0.1)
+            # if x < x_medium:
+            #     print("left")
+            #     motor.move_non_threaded(1, "left")
+            #     #time.sleep(0.1)
+            # elif x == x_medium:
+            #     print("stay")
+            # else:
+            #     print("right")
+            #     motor.move_non_threaded(1, "right")
+            #     #time.sleep(0.1)
 
 
             break
+        
+    return frame
+
+while True:
+    frame = cap.read()
+    
+    frame = cv2.flip(frame, 1)
+
+    frame = trackcolor(frame)
 
 
     cv2.imshow("Frame", frame)
